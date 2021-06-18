026e24ecf2848077fd9c73ed40afd2e733319cab
Interactoma <cervantes@ciencias.unam.mx>
2021-06-18 16:38:36 -0500

    ORG L01, & ADD L02

---
+++ Leccion02/02-01-TiposDatosJavaScript.js
@@ -0,0 +1,16 @@
+//Tipo de dato string
+var nombre = "Carlos";
+console.log(nombre);
+
+//Tipo de datos numerico
+var numero = 1000;
+console.log(numero);
+
+//Tipo de dato object
+var objeto = {
+    nombre : "Juan",
+    apellido : "perez",
+    telefono : "5525251414"
+}
+
+console.log(objeto);
